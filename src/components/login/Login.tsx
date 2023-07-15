7eedc1cadb89b6a60dc2887e18c992cf7dba7f77
Abdul-jabbar Hersi <hersi.abduljabbar@gmail.com>
2023-07-15 16:30:16 -0400

    Saving work 2

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,8 +1,29 @@
 import linktreeLogo from '../../assets/linktree-logo.svg'
 import './Login.css'
 
+import { useState } from 'react';
+import firebase from 'firebase';
+import { MouseEvent } from 'react';
+
 function Login (){
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const signIn = firebase.auth().signInWithEmailAndPassword;
+  
+  const submitLogin = (e: MouseEvent) => {
+    console.log(email,password)
+    e.preventDefault();
+    signIn(email, password).then((userCredential: any)=> {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(userCredential)
+    })
+    .catch((error: Error) => {
+      const errorMessage = error.message;
+      console.error(errorMessage)
+    });
+  }
 
   return (
     <div className='login-container'>
@@ -10,9 +31,25 @@ function Login (){
       <div className='login'>
         <h1>Log into your linktree</h1>
         <form className='login-form'>
-          <input className='input' type="text" name="username" placeholder='username' />
-          <input className='input' type="password" name="password" placeholder='password'/>
-          <button className='login-button'>Log In</button>
+          <input 
+            className='input' 
+            type="text" 
+            name="email" 
+            placeholder='email' 
+            onChange={(event) =>
+              setEmail(event.target.value)
+            }
+          />
+          <input 
+            className='input' 
+            type="password" 
+            name="password" 
+            placeholder='password'
+            onChange={(event) =>
+              setPassword(event.target.value)
+            }
+          />
+          <button type='submit' className='login-button' onClick={submitLogin}>Log In</button>
         </form>
       </div>
     </div> 
